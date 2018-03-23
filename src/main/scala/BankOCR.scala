111065eb95cdcab5a26068945b86ffc0bd6e1350
jennygrahamjones <jenny.grahamjones@gmail.com>
2018-03-23 13:18:52 +0000

    Add checksum evaluation and handling of illegible characters - Where numbers are illegibe, a ? will be returned instead of a number - Where all numbers are legible, the checksum will be evaluated - Error messages are displayed alongside returns

--- src/main/scala/BankOCR.scala
+++ src/main/scala/BankOCR.scala
@@ -1,3 +1,5 @@
+import scala.util.{Failure, Success, Try}
+
 object BankOCR {
 
   val pattern = Map(" _ " +
@@ -6,6 +8,9 @@ object BankOCR {
                     "   " +
                     " | " +
                     " | " -> "1",
+                    "   " +
+                    "  |" +
+                    "  |" -> "1",
                     " _ " +
                     " _|" +
                     "|_ " -> "2",
@@ -29,32 +34,54 @@ object BankOCR {
                     "|_|" -> "8",
                     " _ " +
                     "|_|" +
-                    "  |" -> "9"
+                    " _|" -> "9"
                     )
 
-  case class CR(top: String, middle: String, bottom: String) {
+  def readInput(input: String): String = {
+    val accountNumberAsDigits: String = AccountNumberInput(input.substring(0, 27), input.substring(27, 54), input.substring(54, 81)).digits
+    if(accountNumberAsDigits.contains("?")) accountNumberAsDigits + " ILL"
+    else Try(evaluateChecksum(accountNumberAsDigits)) match {
+      case Success(_) => accountNumberAsDigits
+      case Failure(_) => accountNumberAsDigits + " ERR"
+    }
+  }
+
+  def evaluateChecksum(input: String): Boolean = {
+    val str: List[Int] = input.map(_.toString).map(_.toInt).toList
+    val strFormattedAsChecksum = Checksum(str.head,str(1),str(2),str(3),str(4),str(5),str(6),str(7),str(8))
+    if(strFormattedAsChecksum.isValid) true else throw new InvalidChecksumException("checksum invalid")
+  }
+
+  class InvalidChecksumException(msg: String) extends Exception(msg)
+
+  case class Checksum(nine: Int, eight: Int, seven: Int, six: Int, five: Int, four: Int, three: Int, two: Int, one: Int) {
+    def isValid: Boolean = {
+      ((one + 2) * (two + 3) * (three + 4) * (four + 5) * (five + 6) * (six + 7) * (seven + 8) * (eight + 9) * nine) % 11 == 0
+    }
+  }
+
+  case class AccountNumberInput(top: String, middle: String, bottom: String) {
     override def toString: String = top + middle + bottom
 
-    def digitiser(start: Int, end: Int): String =
-      top.substring(start,end) + middle.substring(start,end) + bottom.substring(start,end)
+    def digitiser(start: Int, end: Int): String = {
+      val num: String = top.substring(start, end) + middle.substring(start, end) + bottom.substring(start, end)
+      val tryPattern: Try[String] = Try(pattern(num))
 
-    val firstDigit: String = digitiser(0,3)
-    val secondDigit: String = digitiser(3,6)
-    val thirdDigit: String = digitiser(6,9)
-    val fourthDigit: String = digitiser(9,12)
-    val fifthDigit: String = digitiser(12,15)
-    val sixthDigit: String = digitiser(15,18)
-    val seventhDigit: String = digitiser(18,21)
-    val eighthDigit: String = digitiser(21,24)
-    val ninthDigit: String = digitiser(24,27)
+      tryPattern match {
+        case Success(_) => pattern(num)
+        case Failure(_) => "?"
+      }
+    }
 
-    val digits: String = pattern(this.firstDigit) + pattern(this.secondDigit) + pattern(this.thirdDigit) +
-      pattern(this.fourthDigit) + pattern(this.fifthDigit) + pattern(this.sixthDigit) +
-      pattern(this.seventhDigit) + pattern(this.eighthDigit) + pattern(this.ninthDigit)
+    val firstDigit: String = digitiser(0, 3)
+    val secondDigit: String = digitiser(3, 6)
+    val thirdDigit: String = digitiser(6, 9)
+    val fourthDigit: String = digitiser(9, 12)
+    val fifthDigit: String = digitiser(12, 15)
+    val sixthDigit: String = digitiser(15, 18)
+    val seventhDigit: String = digitiser(18, 21)
+    val eighthDigit: String = digitiser(21, 24)
+    val ninthDigit: String = digitiser(24, 27)
+    val digits: String = firstDigit + secondDigit + thirdDigit + fourthDigit + fifthDigit + sixthDigit + seventhDigit + eighthDigit + ninthDigit
   }
-
-  def readInput(input: String): String = {
-    CR(input.substring(0, 27), input.substring(27, 54), input.substring(54, 81)).digits
-  }
-
 }
--- src/test/scala/BankOCRSpec.scala
+++ src/test/scala/BankOCRSpec.scala
@@ -8,23 +8,60 @@ class BankOCRSpec extends WordSpec with MustMatchers {
 
       "return 00000000 when given the corresponding input" in {
         val input = " _  _  _  _  _  _  _  _  _ " +
-                    "| || || || || || || || || |" +
-                    "|_||_||_||_||_||_||_||_||_|"
+          "| || || || || || || || || |" +
+          "|_||_||_||_||_||_||_||_||_|"
         BankOCR.readInput(input) mustEqual "000000000"
-      }
-
-      "return 111111111 when given the corresponding input" in {
-        val input = "                           " +
-                    " |  |  |  |  |  |  |  |  | " +
-                    " |  |  |  |  |  |  |  |  | "
-        BankOCR.readInput(input) mustEqual "111111111"
       }
 
       "return 123456789 when given the corresponding input" in {
         val input = "    _  _     _  _  _  _  _ " +
-                    " |  _| _||_||_ |_   ||_||_|" +
-                    " | |_  _|  | _||_|  ||_|  |"
+          " |  _| _||_||_ |_   ||_||_|" +
+          " | |_  _|  | _||_|  ||_| _|"
         BankOCR.readInput(input) mustEqual "123456789"
+      }
+
+      "return 111111111 ERR when given the corresponding input" in {
+        val input = "                           " +
+          " |  |  |  |  |  |  |  |  | " +
+          " |  |  |  |  |  |  |  |  | "
+        BankOCR.readInput(input) mustEqual "111111111 ERR"
+      }
+
+      "return '49006771? ILL' when given the corresponding input" in {
+        val input = "    _  _  _  _  _  _     _ " +
+          "|_||_|| || ||_   |  |  | _ " +
+          "  | _||_||_||_|  |  |  | _|"
+        BankOCR.readInput(input) mustEqual "49006771? ILL"
+      }
+
+      "return 1234?678? ILL when given the corresonding input" in {
+        val input = "    _  _     _  _  _  _  _ " +
+          "  | _| _||_| _ |_   ||_||_|" +
+          "  ||_  _|  | _||_|  ||_| _ "
+        BankOCR.readInput(input) mustEqual "1234?678? ILL"
+      }
+
+    }
+
+    "checkSum is called" must {
+
+      "return true if the account number (123456789) has a valid checksum" in {
+        val input = "123456789"
+        BankOCR.evaluateChecksum(input) mustEqual true
+      }
+
+      "return false if the account number (232323235) does not have a valid checksum" in {
+        val input = "232323235"
+        intercept[Exception] {
+          BankOCR.evaluateChecksum(input)
+        }
+      }
+
+      "return false if the account number (111111111) does not have a valid checksum" in {
+        val input = "111111111"
+        intercept[Exception] {
+          BankOCR.evaluateChecksum(input)
+        }
       }
 
     }
