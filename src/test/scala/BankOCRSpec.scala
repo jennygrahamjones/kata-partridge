91fe7fffa9c2c1d5ec89726b72d84a40e20a47ba
jennygrahamjones <jenny.grahamjones@gmail.com>
2018-03-26 13:16:21 +0100

    Add extra line to the bottom of input strings in all test cases

--- src/test/scala/BankOCRSpec.scala
+++ src/test/scala/BankOCRSpec.scala
@@ -9,35 +9,40 @@ class BankOCRSpec extends WordSpec with MustMatchers {
       "return 00000000 when given the corresponding input" in {
         val input = " _  _  _  _  _  _  _  _  _ " +
           "| || || || || || || || || |" +
-          "|_||_||_||_||_||_||_||_||_|"
+          "|_||_||_||_||_||_||_||_||_|" +
+          "                           "
         BankOCR.readInput(input) mustEqual "000000000"
       }
 
       "return 123456789 when given the corresponding input" in {
         val input = "    _  _     _  _  _  _  _ " +
           " |  _| _||_||_ |_   ||_||_|" +
-          " | |_  _|  | _||_|  ||_| _|"
+          " | |_  _|  | _||_|  ||_| _|" +
+          "                           "
         BankOCR.readInput(input) mustEqual "123456789"
       }
 
       "return 111111111 ERR when given the corresponding input" in {
         val input = "                           " +
           " |  |  |  |  |  |  |  |  | " +
-          " |  |  |  |  |  |  |  |  | "
+          " |  |  |  |  |  |  |  |  | " +
+          "                           "
         BankOCR.readInput(input) mustEqual "111111111 ERR"
       }
 
       "return '49006771? ILL' when given the corresponding input" in {
         val input = "    _  _  _  _  _  _     _ " +
           "|_||_|| || ||_   |  |  | _ " +
-          "  | _||_||_||_|  |  |  | _|"
+          "  | _||_||_||_|  |  |  | _|" +
+          "                           "
         BankOCR.readInput(input) mustEqual "49006771? ILL"
       }
 
       "return 1234?678? ILL when given the corresonding input" in {
         val input = "    _  _     _  _  _  _  _ " +
           "  | _| _||_| _ |_   ||_||_|" +
-          "  ||_  _|  | _||_|  ||_| _ "
+          "  ||_  _|  | _||_|  ||_| _ " +
+          "                           "
         BankOCR.readInput(input) mustEqual "1234?678? ILL"
       }
 
